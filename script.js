8ea90d9eec8a2abc94d48e504ece17d8e7d6334b
yayastartcode <ordertokoviral@gmail.com>
2024-07-22 11:10:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobmenu = document.querySelector("#mobmenu");
+function openMenu() {
+  mobmenu.style.transform = "translateX(-16rem)";
+}
+function closeMenu() {
+  mobmenu.style.transform = "translateX(16rem)";
+}
